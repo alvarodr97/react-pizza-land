11b0b2c34e10b536b1d96a87ae39d2a092176495
Alvaro <alvarodr_97@hotmail.com>
2024-06-15 23:56:44 +0200

    fix linkbutton

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -1,8 +1,8 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link, LinkProps, useNavigate } from "react-router-dom";
 
 interface Props {
   children: string;
-  to: string;
+  to: LinkProps['to'];
 }
 
 export default function LinkButton({ children, to }: Props) {
